92bf161e3ff76520c197acc3840b53381d451650
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-26 21:37:40 +0200

    Remove commented-out code and retain user prompt functionality

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("elsó sor")
-data="other"
-val = input(f"második sor {data}")
-print("harmadik sor", val)
+# print("elsó sor")
+# data="other"
+# val = input(f"második sor {data}")
+# print("harmadik sor", val)
+
+while True:
+    print("kérlek válassz")
+    if input("adj meg egy karaktert: ").lower() == 'k':
+        break
